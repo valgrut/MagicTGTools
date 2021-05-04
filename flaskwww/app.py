a31ed51c564e09f21814d4749846ac708f069904
valgrut <naruto987@seznam.cz>
2021-05-04 03:24:16 +0200

    Moved html files into template folder to fix errors that my html files cant be found

--- flaskwww/app.py
+++ flaskwww/app.py
@@ -1,22 +1,23 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to the <b>home</b> page"
+    return render_template("index.html")
 
-@app.route("/hi/")
-def who():
-    return "Who are you?"
+@app.route("/login/<name>")
+def who(name):
+    return render_template("login.html", content=name)
+    # return "Who are you?"
 
 @app.route("/hi/<username>")
 def greet(username):
     return f"Hi there, {username}!"
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
+# @app.route("/admin")
+# def admin():
+    # return redirect(url_for("home"))
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+    # app.run()
