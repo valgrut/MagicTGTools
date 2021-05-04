2b93fee05e78c3effa6343451207e486b191e8ee
valgrut <naruto987@seznam.cz>
2021-05-04 02:57:49 +0200

    Added makefile target to start server with file-changes auto checking.

--- flaskwww/app.py
+++ flaskwww/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Welcome to the index page"
+def home():
+    return "Welcome to the <b>home</b> page"
 
 @app.route("/hi/")
 def who():
@@ -13,3 +13,10 @@ def who():
 @app.route("/hi/<username>")
 def greet(username):
     return f"Hi there, {username}!"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run()
