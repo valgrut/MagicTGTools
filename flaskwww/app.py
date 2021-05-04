abd8fd75f1df5a474c923793319b34cd9537f13b
valgrut <naruto987@seznam.cz>
2021-05-04 22:36:03 +0200

    Flask update: File uploading and print cards as a list on html page.

--- flaskwww/app.py
+++ flaskwww/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
 
 # Bootstrap websites: https://getbootstrap.com/docs/5.0/examples/
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 app.permanent_session_lifetime = timedelta(days=5)
+app.config['UPLOAD_FOLDER'] = "./"
+app.config['MAX_CONTENT_PATH'] = "10000"
 
 @app.route("/")
 def home():
@@ -40,6 +44,31 @@ def logout():
     flash("You have been logged out", "info")
     session.pop("user", None)
     return redirect(url_for("login"))
+
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_input_file():
+    if request.method == 'POST':
+        f = request.files['file']
+
+        # f.save(secure_filename(f.filename))
+        f.save(f.filename)
+        flash('Deck has been uploaded successfully')
+
+        # process deck and print urls by given options from form (options like target web etc.)
+        deckfile = open("./" + f.filename, "r")
+        cards = deckfile.read().split('\n')
+        # TODO process and modify card urls based on options from form
+        # ...
+        deckfile.close()
+        return render_template('uploader.html', filename=f.filename, deck=cards)
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
 
 
 # @app.route("/admin")
